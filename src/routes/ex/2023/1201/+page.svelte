54a048e6ad6ba4e01df8183109ebc0adf5299b1b
“emflant” <emflant@gmail.com>
2023-12-02 10:47:37 +0900

    .

--- src/routes/ex/2023/1201/+page.svelte
+++ src/routes/ex/2023/1201/+page.svelte
@@ -4,6 +4,7 @@
     import javascript from 'highlight.js/lib/languages/javascript';
     import julia from 'highlight.js/lib/languages/julia';
     import go from 'highlight.js/lib/languages/go';
+    import 'highlight.js/styles/atom-one-dark.min.css';
 
     // 필요한 언어만 가져다가 바인딩 한다.
     hljs.registerLanguage('go', go);
@@ -11,7 +12,15 @@
     hljs.registerLanguage('julia', julia);
     
 
+    const codeGo =
+`package main
 
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
+`;
 
 const codeJulia = 
 `function f(x, y)
@@ -31,30 +40,26 @@ end
     });
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/atom-one-dark.min.css">
-</svelte:head>
-
 <div><h2 class="my-5">Svelte에서 highlight.js 에 언어정보 배지추가</h2></div>
 
 <div class="row">
     <div class="col-lg-6">
-        <h4>text-bg-warning 테마</h4>
-        <div class="position-relative">
-            <pre><code class="language-julia">{codeJulia}</code>
-            </pre>
-            <h6><span class="position-absolute top-0 end-0 badge text-bg-warning rounded-0 p-2">JAVA</span></h6>
-        </div>
-
         <h4>text-bg-dark 테마</h4>
         <p>그런데 <code>text-bg-dark</code> 가 완벽한 black은 아니어서 <code>bootstrap.css</code> 파일을 수정했다.</p>
         <div class="position-relative">
             <pre><code class="language-julia">{codeJulia}</code>
             </pre>
-            <h6><span class="position-absolute top-0 end-0 badge text-bg-dark rounded-0 p-2">JAVA</span></h6>
+            <h6><span class="position-absolute top-0 end-0 badge text-bg-dark rounded-0 p-2 text-uppercase">Julia</span></h6>
         </div>
     </div>
     <div class="col-lg-6">
+        <h4>text-bg-warning 테마</h4>
+        <p>warning 테마도 어울리는 편 같다. 모서리마다 약간 라운드 처리를 하니 조금 더 부드러워 보인다.</p>
+        <div class="position-relative">
+            <pre><code class="language-go rounded-2">{codeGo}</code>
+            </pre>
+            <h6><span class="position-absolute top-0 end-0 badge text-bg-warning rounded-1 p-2 text-uppercase">go</span></h6>
+        </div>
     </div>
     
 </div>
