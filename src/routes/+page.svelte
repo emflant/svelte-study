c9d46700a4533ecbb05cd8b0fac70e8e0f184da6
“emflant” <emflant@gmail.com>
2023-12-02 09:12:37 +0900

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,19 +8,16 @@
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-
-<div><h2 class="mt-5">Home</h2></div>
-
 <div class="row">
 	<div class="col-md-6">
 		<div class="list-group">
-			<a href="/ex/2023/1115" class="list-group-item list-group-item-action" data-sveltekit-reload>ex-20231115</a>
-			<a href="/ex/2023/1116" class="list-group-item list-group-item-action" data-sveltekit-reload>ex-20231116</a>
-			<a href="/ex/2023/1124" class="list-group-item list-group-item-action" data-sveltekit-reload>Svelte에서 Bootstrap 아이콘 사용하기1</a>
-			<a href="/ex/2023/1125" class="list-group-item list-group-item-action" data-sveltekit-reload>Svelte에서 Bootstrap 아이콘 사용하기2</a>
-			<a href="/ex/2023/1126" class="list-group-item list-group-item-action" data-sveltekit-reload>Svelte에서 Bootstrap 아이콘 사용하기3</a>
-			<a href="/ex/2023/1127" class="list-group-item list-group-item-action" data-sveltekit-reload>highlightjs 예시1</a>
-			<a href="/ex/2023/1128" class="list-group-item list-group-item-action" data-sveltekit-reload>highlightjs 예시2</a>
+			<a href="/ex/2023/1115" class="list-group-item list-group-item-action" rel="external">ex-20231115</a>
+			<a href="/ex/2023/1116" class="list-group-item list-group-item-action">ex-20231116</a>
+			<a href="/ex/2023/1124" class="list-group-item list-group-item-action">Svelte에서 Bootstrap 아이콘 사용하기1</a>
+			<a href="/ex/2023/1125" class="list-group-item list-group-item-action">Svelte에서 Bootstrap 아이콘 사용하기2</a>
+			<a href="/ex/2023/1126" class="list-group-item list-group-item-action">Svelte에서 Bootstrap 아이콘 사용하기3</a>
+			<a href="/ex/2023/1127" class="list-group-item list-group-item-action">highlightjs 예시1</a>
+			<a href="/ex/2023/1128" class="list-group-item list-group-item-action">highlightjs 예시2</a>
 			<a href="/ex/2023/1129" class="list-group-item list-group-item-action" data-sveltekit-reload>highlightjs 예시 - 1129</a>
 			<a href="/ex/2023/1130" class="list-group-item list-group-item-action" data-sveltekit-reload>highlightjs 예시 - 1130</a>
 			<a href="/ex/2023/1131" class="list-group-item list-group-item-action" data-sveltekit-reload>highlightjs 예시 - 1131</a>
