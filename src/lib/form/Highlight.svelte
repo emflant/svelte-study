ff1f8426629d67995b32ae5b36ba5b8960fe6282
“emflant” <emflant@gmail.com>
2023-12-05 21:57:27 +0900

    .

--- src/lib/form/Highlight.svelte
+++ src/lib/form/Highlight.svelte
@@ -7,5 +7,5 @@
     <pre><code class="language-{language} rounded-3">{code}</code>
     </pre>
     <h6><span class="position-absolute top-0 end-0 
-        badge text-bg-warning rounded-3 rounded-bottom-0 rounded-start-0 py-2 px-2 text-uppercase">{language}</span></h6>
+        badge text-bg-warning rounded-2 py-2 px-2 text-uppercase">{language}</span></h6>
 </div>
