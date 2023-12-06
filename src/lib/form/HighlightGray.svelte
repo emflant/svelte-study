8867f1b2144c6a35c4030c2a69674ab993c434d8
“emflant” <emflant@gmail.com>
2023-12-06 21:53:34 +0900

    .

--- src/lib/form/HighlightGray.svelte
+++ src/lib/form/HighlightGray.svelte
@@ -1,12 +1,19 @@
 <script>
     export let code;
     export let language;
+
+    function aa(){
+        alert('111');
+    }
 </script>
 <div class="position-relative m-0">
     <pre><code class="language-{language} rounded-3">{code}</code></pre>
     <span class="position-absolute top-0 end-0 
         badge rounded-3 py-2 px-2 text-uppercase" 
-        style="color:#C0C0C0">{language}</span>
-    <!-- <span class="position-absolute top-50 end-0"><i class="bi bi-copy" 
-        style="font-size: 1rem;margin-right:0.3em; color: white;"></i></span> -->
+        style="color:#C0C0C0;margin-right: 0em;margin-top:0em">{language}</span>
+    <button class="btn p-0 position-absolute bottom-0 end-0" 
+        style="margin-right: 0.4em;margin-bottom: 0.3em;"
+        on:click={aa}>
+        <i class="bi bi-copy" 
+        style="font-size: 1rem;color: #C0C0C0;-webkit-text-stroke-width:0.04em"></i></button>
 </div>
