7beb387bb71aba93f79371f6dc35aecc563f4a1e
“emflant” <emflant@gmail.com>
2023-12-09 09:29:58 +0900

    .

--- src/routes/ex/2023/1204/+page.svelte
+++ src/routes/ex/2023/1204/+page.svelte
@@ -1,52 +1,25 @@
 <script>
     import { onMount } from 'svelte';
-    import HighlightPop from '$lib/form/HighlightPop.svelte';
 
     import hljs from 'highlight.js/lib/core';
     import typescript from 'highlight.js/lib/languages/typescript';
     import julia from 'highlight.js/lib/languages/julia';
-    import go from 'highlight.js/lib/languages/go';
-    import makefile from 'highlight.js/lib/languages/makefile';
+    import xml from 'highlight.js/lib/languages/xml';
 
     import 'highlight.js/styles/atom-one-dark.min.css';
     import 'bootstrap-icons/font/bootstrap-icons.css'
 
     // 필요한 언어만 가져다가 바인딩 한다.
-    hljs.registerLanguage('go', go);
     hljs.registerLanguage('typescript', typescript);
     hljs.registerLanguage('julia', julia);
-    hljs.registerLanguage('makefile', makefile);
+    hljs.registerLanguage('xml', xml);
     
-    const codeGo =
-`package main
-
-import "fmt"
-
-func main() {
-    fmt.Println("Hello, 世界")
-}`;
-
 const codeJulia = 
 `function f(x, y)
     x[1] = 42    # mutates x
     y = 7 + y    # new binding for y, no mutation
     return y
 end`;
-
-    const codeMakefile =
-`# Makefile
-
-BUILDDIR      = _build
-EXTRAS       ?= $(BUILDDIR)/extras
-
-.PHONY: main clean
-
-main:
-	@echo "Building main facility..."
-	build_main $(BUILDDIR)
-
-clean:
-	rm -rf $(BUILDDIR)/*`;
 
     const codeTypescript = 
 `class MyClass {
@@ -78,8 +51,16 @@ myArray.forEach(() => { }); // fat arrow syntax`;
 <div><h2 class="my-5">pre 태그 공백</h2></div>
 
 <div class="row">
-    <div class="col-lg-6">
-        <h4>불필요한 공백이 있는 경우</h4>
+    <div class="col-lg-6 mb-5">
+        <h4 class="mb-4">불필요한 공백이 있는 경우</h4>
+        <p>pre 태그를 아래와 같이 이쁘게 줄바꿈을 넣어가며 정리하는 순간,</p>
+        <pre><code class="language-xml">&lt;pre style=&quot;border:3px red solid&quot;&gt;
+    &lt;code class=&quot;language-typescript&quot;&gt;codeTypescript&lt;/code&gt;
+&lt;/pre&gt;
+&lt;pre style=&quot;border:3px red solid&quot;&gt;
+    &lt;code class=&quot;language-julia&quot;&gt;codeJulia&lt;/code&gt;
+&lt;/pre&gt;</code></pre>
+        <p>결과는 아래와 같다. 불필요한 공간이 생기게 된다.</p>
         <pre style="border:3px red solid">
             <code class="language-typescript">{codeTypescript}</code>
         </pre>
@@ -87,8 +68,12 @@ myArray.forEach(() => { }); // fat arrow syntax`;
             <code class="language-julia">{codeJulia}</code>
         </pre>
     </div>
-    <div class="col-lg-6">
-        <h4>불필요한 공백이 없는 경우</h4>
+    <div class="col-lg-6 mb-5">
+        <h4 class="mb-4">불필요한 공백이 없는 경우</h4>
+        <p>pre 태그내에서는 아래와 같이 공백없이 코딩해야,</p>
+        <pre><code>&lt;pre style=&quot;border:3px red solid&quot;&gt;&lt;code class=&quot;language-typescript&quot;&gt;codeTypescript&lt;/code&gt;&lt;/pre&gt;
+&lt;pre style=&quot;border:3px red solid&quot;&gt;&lt;code class=&quot;language-julia&quot;&gt;codeJulia&lt;/code&gt;&lt;/pre&gt;</code></pre>
+        <p>쓸데없는 공간이 생기지 않는다.</p>
         <pre style="border:3px red solid"><code class="language-typescript">{codeTypescript}</code></pre>
         <pre style="border:3px red solid"><code class="language-julia">{codeJulia}</code></pre>
     </div>
